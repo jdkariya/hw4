73d9888e18c971232dcda9b5109583852bf77f49
jdkariya <josh.kariya@kellogg.northwestern.edu>
2022-05-30 04:03:38 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
+  get "/logout", :controller => "sessions", :action => "destroy"
+  get "/login", :controller => "sessions", :action => "new"
   resources "places"
   resources "posts"
   resources "sessions"
