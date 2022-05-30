130b24de31187094176ecfdbc2ccfee71c449338
jdkariya <josh.kariya@kellogg.northwestern.edu>
2022-05-30 03:25:31 +0000

    Update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,26 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # login the user
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Login Successful"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful Login"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Unsuccessful Login"
+      redirect_to "/login"
+    end
   end
-
+  
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
